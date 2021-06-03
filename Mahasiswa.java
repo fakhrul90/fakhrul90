02808fe03fed94c01a88e568c96a0ff629723de8
fakhrul90 <fakhrulrazi892@gmail.com>
2021-06-03 11:27:38 +0700

    main

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -1,59 +1,82 @@
 /**
- * class Mahasiswa.
+ * class Mahasiswa 
  *
- * @author Fakhrul Razi
- * @version 0.1
+ * @author fakhrul razi
+ * @version  0.02
  */
 public class Mahasiswa
 {
-    // instance variables - replace the example below with your own
+      // instance variables - replace the example below with your own
     private int nim;
-    private String nama;
-    private String kelas;
-    private String alamat;
     private int umur;
+    private String nama; 
+    private String alamat;
+    private String kelas;
     
-    public Mahasiswa(int nim, String nama, String kelas,String alamat, int umur)
+    /**
+     * Constructor for objects of class Mahasiswa
+     */
+    public Mahasiswa(int nim, int umur, String nama, String alamat, String kelas)
     {
         this.nim = nim;
-        this.nama = nama;
-        this.kelas = kelas;
-        this.alamat = alamat;
         this.umur = umur;
-                
+        this.nama = nama;
+        this.alamat = alamat;
+        this.kelas = kelas;
     }
-   
-    public int getnimMhs()
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public String getnamaMhs()
     {
-         return this.nim;
-    }
-    
-   public String getnamaMhs()
-    {
-         return this.nama;
-    }
-    
-    public String getkelasMhs()
-    {
-         return this.kelas;
+        return this.nama;
     }
     
     public String getalamatMhs()
     {
-         return this.alamat;
+        return this.alamat;
     }
-     
+    
+    public String getkelasMhs()
+    {
+        return this.kelas;
+    }
+    
+    public int getnimMhs()
+    {
+        return this.nim;
+    }
+    
     public int getumurMhs()
     {
-         return this.umur;
+        return this.umur;
     }
     
-    public void setnamaMhs(String nama)
-    {
+    public void setnamaMhs(String nama){
         this.nama = nama;
     }
+      public void setalamatMhs(String alamat){
+        this.alamat = alamat;
+    }
     
-
-
+    public void setkelasMhs(String kelas){
+        this.kelas = kelas;
+    }
+    
+    public void setnimMhs(int nim){
+        this.nim = nim;
+    }
+    
+    public void setumurMhs(int umur){
+        this.umur = umur;
+    }
+   
+    
+  
+    
     
 }
--- Main.java
+++ Main.java
@@ -3,130 +3,152 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.List;
 /**
- * class Main.
+ * class Main 
  *
- * @author Fakhrul Razi
- * @version 0.1
+ * @author   fakhrul razi
+ * @version  0.02
  */
 public class Main
 {
-    public static void main(String args[])
-    {
-                
-        ArrayList<Mahasiswa> arrm = new ArrayList<Mahasiswa>();
+    public static void main(String args[]){
         
-              
-        // arrm.add(new Mahasiswa(1001,"Doni"));
-        // arrm.add(new Mahasiswa(1002,"Bayu"));
-        // arrm.add(new Mahasiswa(1003,"Fikri"));
+        //ArrayList<Integer> arri = new ArrayList<Integer>();
+        //ArrayList<String> arrs = new ArrayList<String>();
+        ArrayList<Mahasiswa> arrm = new ArrayList<Mahasiswa>();
         
         System.out.println("Data Mahasiswa");
         System.out.print("Jumlah Mahasiswa: ");
         Scanner input = new Scanner(System.in);
-        int jumlah=0, nim=0, umur=0;
-        String nama=null, kelas=null, alamat=null;
+        int jumlah=0; 
+        int nim=0;
+        int umur=0;
+        String nama=null;
+        String alamat=null;
+        String kelas=null;
         jumlah = input.nextInt();
         
-        for (int i=0; i < jumlah ; i++) 
+        
+        //Menginput Data Mahasiswa
+        for(int i=0; i < jumlah; i++)
         {
-            System.out.print("nim mahasiswa ke-"+(i+1)+": ");
+            
+            System.out.print("Nim ke-"+(i+1)+": ");
             nim = input.nextInt();
-            System.out.print("nama mahasiswa ke-"+(i+1)+": ");
+            System.out.print("Nama ke-"+(i+1)+": ");
             nama = input.next();
-            System.out.print("kelas mahasiswa ke-"+(i+1)+":  ");
-            kelas = input.next();
-            System.out.print("alamat mahasiswa ke-"+(i+1)+":  ");
+            System.out.print("Alamat "+(i+1)+": ");
             alamat = input.next();
-            System.out.print("umur mahasiswa ke-"+(i+1)+":  ");
+            System.out.print("Kelas "+(i+1)+": ");
+            kelas = input.next();
+            System.out.print("Umur "+(i+1)+": ");
             umur = input.nextInt();
-
-
-            arrm.add(new Mahasiswa(nim, nama, kelas, alamat, umur));
-        }
-        
-        
-        System.out.println("Menampilkan Data Mahasiswa");
-        for (Mahasiswa mhs: arrm)
-        {
-              System.out.println("nim = "+ mhs.getnimMhs());
-              System.out.println("nama = "+ mhs.getnamaMhs());
-              System.out.println("kelas = "+ mhs.getkelasMhs());
-              System.out.println("alamat = "+ mhs.getalamatMhs());
-              System.out.println("umur = "+ mhs.getumurMhs());
-        }
-        System.out.println("\nUpdate Data Mahasiswa");
-        int cariNim;
-        System.out.print("Cari NIM = ");
-        cariNim = input.nextInt();
-        for(Mahasiswa mhs:arrm)
-        {
-        if (mhs.getnimMhs() == cariNim)
-        {
-        System.out.print("Set Nama = ");    
-        nama = input.next();
-        mhs.setnamaMhs(nama);
-        }
-        }
-         
-        System.out.println("\nData Mahasiswa Setelah di Update");
-        for(Mahasiswa mhs:arrm)
-        {
-            System.out.println("nim = "+ mhs.getnimMhs());
-             System.out.println("nama = "+ mhs.getnamaMhs());
-              System.out.println("kelas = "+ mhs.getkelasMhs());
-             System.out.println("alamat = "+ mhs.getalamatMhs());
-              System.out.println("umur = "+ mhs.getumurMhs());
+            arrm.add(new Mahasiswa(nim,umur,nama,alamat,kelas));
+            System.out.println("\n");
             
         }
         
+        
+        //Menampilkan Hasil Input
+        System.out.println("\n");
+        System.out.println("Menampilkan Data Mahasiswa");
+        for (Mahasiswa mhs : arrm)
+        {
+            System.out.println("Nim : "+mhs.getnimMhs());
+            System.out.println("Nama : "+mhs.getnamaMhs());
+            System.out.println("Alamat : "+mhs.getalamatMhs());
+            System.out.println("Kelas : "+mhs.getkelasMhs());
+            System.out.println("Umur : "+mhs.getumurMhs());
+            System.out.println("---------------------");
+        }
+        
+        //Mencari data Mahasiswa
         System.out.println("\nSearch Data Mahasiswa");
-        System.out.print("Cari NIM = ");
+        int cariNim;
+        System.out.print("Cari NIM: ");
         cariNim = input.nextInt();
-        System.out.println("\nData Mahasiswa setelah di search");
-        for(Mahasiswa mhs:arrm)
+        System.out.println("Hasil yang dicari:");
+        for (Mahasiswa mhs : arrm)
         {
-        if (mhs.getnimMhs() == cariNim)
-        {
-        System.out.println("nim = "+ mhs.getnimMhs());
-             System.out.println("nama = "+ mhs.getnamaMhs());
-              System.out.println("kelas = "+ mhs.getalamatMhs());
-             System.out.println("alamat = "+ mhs.getalamatMhs());
-              System.out.println("umur = "+ mhs.getumurMhs());
+            if(mhs.getnimMhs() == cariNim){
+                System.out.println("Nim : "+mhs.getnimMhs());
+                System.out.println("Nama : "+mhs.getnamaMhs());
+                System.out.println("Alamat : "+mhs.getalamatMhs());
+                System.out.println("Kelas : "+mhs.getkelasMhs());
+                System.out.println("Umur : "+mhs.getumurMhs());
+            }
         }
-        }
-        
-        
-        System.out.println("\nMemeriksa apakah array list kosong: ");
-        System.out.println(arrm.isEmpty()); 
-        
-        System.out.println("\nMemeriksa ada berapa array list: ");
-        System.out.println(arrm.size());
-        
-        System.out.println("\nRemove Data Mahasiswa");
-        System.out.print("Masukkan NIM = ");
+    
+        //Mengupdate data Mahasiswa
+        System.out.println("\nUpdate Data Mahasiswa");
+        System.out.print("Nim Yang Di Update: ");
         cariNim = input.nextInt();
-        for(Mahasiswa mhs:arrm)
+        for (Mahasiswa mhs : arrm)
         {
-        if (mhs.getnimMhs() == cariNim)
+            if(mhs.getnimMhs() == cariNim)
+            {
+                System.out.print("Nama : ");
+                nama = input.next();
+                
+                
+                
+                System.out.print("Alamat : ");
+                alamat = input.next();
+               
+                
+                System.out.print("Kelas : ");
+                kelas = input.next();
+               
+                
+                System.out.print("Umur : ");
+                umur = input.nextInt();
+                
+                arrm.add(new Mahasiswa(nim,umur,nama,alamat,kelas));
+                System.out.println("\n");
+            
+               
+            }
+            
+        }
+        
+        //Menampilkan Hasil Update
+        System.out.println("\n");
+        System.out.println("Data Setelah di Update");
+        for (Mahasiswa mhs : arrm)
         {
-        arrm.indexOf(mhs);
-        arrm.remove(arrm.indexOf(mhs));
-        break;
-        }
+            System.out.println("Nim : " +mhs.getnimMhs());
+            System.out.println("Nama : " +mhs.getnamaMhs());
+            System.out.println("Alamat : "+mhs.getalamatMhs());
+            System.out.println("Kelas : "+mhs.getkelasMhs());
+            System.out.println("Umur : "+mhs.getumurMhs());
+            System.out.println("-----------------------------");
         }
         
-        System.out.println("\nData Mahasiswa Setelah di Hapus");
-        for(Mahasiswa mhs:arrm)
+        //Menghapus data Mahasiswa
+        System.out.println("\nMenghapus Data Mahasiswa");
+        System.out.print("NIM Yang Ingin DiHapus: ");
+        cariNim = input.nextInt();
+        for (Mahasiswa mhs : arrm)
         {
-             System.out.println("nim = "+ mhs.getnimMhs());
-             System.out.println("nama = "+ mhs.getnamaMhs());
-             System.out.println("kelas = "+ mhs.getkelasMhs());
-             System.out.println("alamat = "+ mhs.getalamatMhs());
-             System.out.println("umur = "+ mhs.getumurMhs());
+            if(mhs.getnimMhs() == cariNim){
+                arrm.indexOf(mhs);
+                arrm.remove(arrm.indexOf(mhs));
+            }
         }
-    } 
         
+        //Menampilkan Hasil Update
+        System.out.println("\n");
+        System.out.println("Data Setelah di Dihapus");
+        for (Mahasiswa mhs : arrm)
+        {
+            System.out.println("Nim : " +mhs.getnimMhs());
+            System.out.println("Nama : " +mhs.getnamaMhs());
+            System.out.println("Alamat : "+mhs.getalamatMhs());
+            System.out.println("Kelas : "+mhs.getkelasMhs());
+            System.out.println("Umur : "+mhs.getumurMhs());
+            System.out.println("-----------------------------");
+        }
         
-        
-        
+       
+    }
+    
 }
